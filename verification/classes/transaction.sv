4bda0266f34cdb7d826cceffd09dc0136862d2c7
Mohamed Safwat <safwat.m1662002@gmail.com>
2026-05-05 03:32:58 +0300

    Add transaction class with relevant fields

--- verification/classes/transaction.sv
+++ verification/classes/transaction.sv
@@ -1,1 +1,10 @@
-
+class transaction;
+    bit                   rst_n;
+    bit                   valid_data;
+    bit                   en_op;
+    bit      [127:0]      input_activations;
+    bit      [23:0]       new_three_col_C;
+    bit      [23:0]       new_three_col_D;
+    bit      [7:0]        filter_size;
+    bit                   initial_window;
+endclass
